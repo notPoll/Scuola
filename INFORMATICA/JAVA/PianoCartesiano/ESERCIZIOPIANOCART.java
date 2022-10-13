173c3b34eba5ae738a725e38e9a43ea99c540a10
notPoll <89387680+notPoll@users.noreply.github.com>
2022-10-13 17:39:03 +0200

    up

--- INFORMATICA/JAVA/PianoCartesiano/ESERCIZIOPIANOCART.java
+++ INFORMATICA/JAVA/PianoCartesiano/ESERCIZIOPIANOCART.java
@@ -1,6 +1,6 @@
 package PianoCartesiano;
 import java.io.*;
-public class ESPC {
+public class Punto {
     private double puntox;
     private double puntoy;
     public void setX(double x) {
@@ -19,22 +19,33 @@ public class ESPC {
                             System.out.println("punto x: " + puntox);
                             System.out.println("Punto y: " + puntoy);
                         }
-                                public boolean equalsy(ESPC p2) {
+                                public boolean equalsy(Punto p2) {
                                     if((puntoy==p2.puntoy) && (puntox==p2.puntox)) {
                                         return true;
                                     }
                                     else 
                                         return false;
                                 }
-                                public double Distanza(ESPC p1) {
+                                public double Distanza(Punto p1) {
                                    double d;
                                 d = Math.sqrt(Math.pow(p1.puntox - puntox,2) + Math.pow(p1.puntoy - puntoy,2));
                                 return d;
                                 }
 }
-class MainPunto {
+ public class MainPianoCartesiano {
     public static void main(String[] args) {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader tastiera = new BufferedReader(input);
-    }
+        double x1,x2,y1,y2;
+        Punto p1,p2;
+        System.out.println("Inserisci la X");
+        try {
+            string Numero = tastiera.readLine();
+                
+        } 
+        catch (Exception e) {}
+        x1 = 
+        
+        }   
+        
 }
