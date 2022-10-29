eb1b86cdc7445ce97c50e877ae394d83cdf3b459
notPoll <89387680+notPoll@users.noreply.github.com>
2022-10-29 11:16:22 +0200

    up

--- TPSI 4CINF/grande.sh
+++ TPSI 4CINF/grande.sh
@@ -3,11 +3,14 @@ b=$2
 FILE=$3
 if [ $a -gt $b ]
 then
-if [ -e $FILE ]
-then
-cat "$FILE"
-else
-ls -l
+    echo " $a è maggiore di $b"
+    if [ -e $FILE ]
+    then
+        echo " stampo del contenuto del file"
+        cat "$FILE"
+        else
+        ls -l
+    fi
+else echo " errore $b è maggiore di $a"
 fi
-else echo " $b è maggiore di $a"
-fi
+read -p "Ho messo in pausa"
