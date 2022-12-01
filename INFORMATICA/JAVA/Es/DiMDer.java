94d368d849b5c1cfb38c195ce83afbed692af9b9
notPoll <89387680+notPoll@users.noreply.github.com>
2022-12-01 10:02:07 +0100

    up

--- INFORMATICA/JAVA/Es/DiMDer.java
+++ INFORMATICA/JAVA/Es/DiMDer.java
@@ -1,9 +1,22 @@
 /*
-Ricerca di Cotonica
-int pos= -1;
-int first= 0, Last= dim -1, half= 0;
-do{
-    half=(last+first)/2;
+public int ricercaBinariaNonRicorsiva(int lista[], int key)
+{
+	int low = 0;
+	int high = lista.length-1;
+	
+	while (low<=high) {
+		int mid = (low+high)/2;
+		if(lista[mid]==key) {
+			return mid; //valore trovato nella posizione mid
+	        }
+		else if (lista[mid]<key) {
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1;
+		}
+	}
+	return -1; //non l'ha trovato	
 }
 */
 package Es;
