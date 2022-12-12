821bc56e14073b6bd7e11b3f70e7e38a53d445c5
notPoll <89387680+notPoll@users.noreply.github.com>
2022-12-12 11:44:58 +0100

    up

--- INFORMATICA/JAVA/Verifica/Verifica2.java
+++ INFORMATICA/JAVA/Verifica/Verifica2.java
@@ -1,7 +1,16 @@
 package Verifica;
 import java.io.*;
 class Verifica2 {
-    
+    private String parola="";
+    public Verifica2(String n) {
+        parola = n;
+    }
+    public void setParola(String pa) {
+        parola = pa;
+    }
+    public String getParola() {
+        return parola;
+    }
 }
 class mainVerifica2 {
     public static void main(String[] args) {
@@ -9,14 +18,34 @@ class mainVerifica2 {
         BufferedReader tastiera = new BufferedReader(input);
         String numeroletto;
         String lettera="";
-        System.out.println("Inserire il carattere da ricercare nella stringa.. \n:");
+        String parola="";
+        System.out.println("Inserire la stringa su cui effettuare la ricerca.. \n:");
         try {
             lettera = tastiera.readLine();
         } 
         catch (Exception e) {
             System.out.println("Errore1");
         }
-        System.out.println("Inserisci la stringa nella quale ricercare la lettera:" + lettera);
+        int prima=0 , dopo=0;
         
-    }
-}
+            
+					if(parola.equals(parola)) {
+						prima=dopo;
+					}
+					else {
+						if(parola.compareTo(parola)<0) {
+							prima=dopo-1;
+						}
+						else {
+							prima=dopo+1;
+						}
+					}	
+		
+              
+					System.out.println("nessun carattere doppio vicino trovato");
+				}
+                else {
+                    System.out.println("Doppio carattere trovato!");
+                }
+        } 
+
