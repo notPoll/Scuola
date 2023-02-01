f3469fe55e47ec3f98dab32962e2e1f8f5559298
notPoll <89387680+notPoll@users.noreply.github.com>
2023-02-01 11:38:53 +0100

    up

--- INFORMATICA/JAVA/GUI/Menu/EsercizioMenu.java
+++ INFORMATICA/JAVA/GUI/Menu/EsercizioMenu.java
@@ -1,4 +1,27 @@
+package Scuola.INFORMATICA.JAVA.GUI.Menu;
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
-class 
+class MainMenu {
+    public static void main(String[] args) {
+        JFrame F = new JFrame();
+        JPanel P = new JPanel();
+        JPanel P1 = new JPanel();
+        JPanel P2 = new JPanel();
+        JPanel P3 = new JPanel();
+        JPanel P4 = new JPanel();
+        JPanel P5 = new JPanel();
+        JButton BOk = new JButton("Ok");
+        JButton BAn = new JButton("Annulla");
+        JLabel Text1 = new JLabel("Inserisci il tuo pranzo");
+        JCheckBox CheckP = new JCheckBox("Primo");
+        JCheckBox CheckP1 = new JCheckBox("Secondo");
+        JLabel Porzioni = new JLabel("Porzioni Primo");
+        JLabel Porzioni2 = new JLabel("Porzioni Secondo");
+        JTextField TPrimo = new JTextField(4);
+        JTextField TSecondo = new JTextField(4);
+        P1.add(Text1);
+        P2.setLayout(new GridLayout(2,2));
+        
+    }
+}
