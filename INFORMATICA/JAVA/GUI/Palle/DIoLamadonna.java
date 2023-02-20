52f30ceadbd551c0d03e1b4f2fac33ffad2216dc
notPoll <89387680+notPoll@users.noreply.github.com>
2023-02-20 11:54:52 +0100

    up

---
+++ INFORMATICA/JAVA/GUI/Palle/DIoLamadonna.java
@@ -0,0 +1,14 @@
+package Scuola.INFORMATICA.JAVA.GUI.Palle;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+class RadioButton extends JFrame implements ActionListener {
+    private JPanel P = new JPanel();
+    private JRadioButton rB1 = new JRadioButton("Radio 1");
+    private JRadioButton rB2 = new JRadioButton("Radio 2");
+    public Button() {
+
+    }
+}
